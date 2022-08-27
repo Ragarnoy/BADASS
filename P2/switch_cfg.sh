4baed4d436a51e587ad979dd4521187c5816ee1d
firelight flagboy <firelight.flagboy@gmail.com>
2022-08-27 17:23:02 +0200

    add script to configure a switch

---
+++ P2/switch_cfg.sh
@@ -0,0 +1,8 @@
+# Configure a switch device.
+# Provide the list of docker container id/name to the script.
+# This script will bridge on `eth\d+` network interface together in a bridge named `br0`.
+set -eux
+
+for host in $*; do
+    docker exec $host bash -c "set -eux; ip link add name br0 type bridge && for eth in \$(ip link show | grep 'eth\d\d*' | sed 's/\d*: eth\(\d*\).*/eth\1/'); do brctl addif br0 \$eth; done && brctl show br0"
+done
