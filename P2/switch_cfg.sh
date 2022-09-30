de249793b227c38ac9780e4ed50174e1613822f8
Youva GAUDE <ygaude@student.42.fr>
2022-09-30 21:20:34 +0200

    improve switch configuration

--- P2/switch_cfg.sh
+++ P2/switch_cfg.sh
@@ -3,6 +3,10 @@
 # This script will bridge on `eth\d+` network interface together in a bridge named `br0`.
 set -eux
 
-for host in $*; do
-    docker exec -i $host bash < switch_net_config.sh
-done
+source config_routers.sh
+
+SWITCH=$(get_router 3)
+
+SWITCH_ID=${SWITCH% *}
+
+docker exec -i $SWITCH_ID bash < switch_net_config.sh
